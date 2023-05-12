b5d3c0b1b6a22fcafd5f4e67d884ee95eed97487
andreneves <andr@andr.com.br>
2023-05-12 20:43:57 -0300

    audit

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Produto extends Model
+
+class Produto extends Model implements Auditable
 {
     use HasFactory;
+    use \OwenIt\Auditing\Auditable;
 
     protected $table = 'produtos';
 
